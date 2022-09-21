6db05f620c32f159c0afe65d35fc1b724e9b04d6
Dương Việt Phát <phatduongvgt@gmail.com>
2022-09-21 23:32:47 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:myshop/ui/products/product_detail_screen.dart';
 import 'package:myshop/ui/products/products_manager.dart';
 import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
+import 'ui/cart/cart_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         ),        
       ),
       home: const SafeArea(
-        child: UserProductsScreen(),
+        child: CartScreen(),
       ),
     );
   }
---
+++ lib/ui/cart/cart_item_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../../models/cart_item.dart';
+import '../shared/dialog_utils.dart';
+
+class CartItemCard extends StatelessWidget {
+  final String productId;
+  final CartItem cartItem;
+
+  const CartItemCard({
+    required this.productId,
+    required this.cartItem,
+    super.key,
+  });
+
+  @override 
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      confirmDismiss: (direction){
+        return showConfirmDialog(
+          context,
+            'Do you want to remove the item from the cart?',
+        );
+      },
+      onDismissed: (direction) {
+        // ignore: avoid_print
+        print('Cart item dismissed');
+      },
+      child: buildItemCard(),
+    );
+  }
+
+  Widget buildItemCard() {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('\$${cartItem.price}'),
+              ),
+            ),
+          ),
+          title: Text(cartItem.title),
+          subtitle: Text('Total: \$${(cartItem.price * cartItem.quantity)}'),
+          trailing: Text('${cartItem.quantity} x'),
+        ),
+      ),
+    );
+  }
+
+  
+}
