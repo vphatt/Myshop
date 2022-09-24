605364e2d2bee93985544ab38a9c92caa6a67b91
Dương Việt Phát <phatduongvgt@gmail.com>
2022-09-25 00:19:18 +0700

    Them thong tin so san pham trong gio hang

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/ui/cart/cart_screen.dart';
+import '../../ui/cart/cart_manager.dart';
+import '../../ui/cart/cart_screen.dart';
+import 'top_right_badge.dart';
 import '../shared/app_drawer.dart';
 
 import 'products_grid.dart';
@@ -31,13 +33,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
     );
   }
   Widget buildShoppingCartIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: () {
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
     );
   }
 
